b9a25cf3eec269cbec3e05c9c1dafb7f8a183f20
iyedexe <iyed.exe@gmail.com>
2018-01-15 13:20:17 +0100

    ID thread handling properly

--- code/test/makethreads.c
+++ code/test/makethreads.c
@@ -16,11 +16,18 @@ else
 }
 int main() {
         SynchPutString("let's begin\n");
-        UserThreadCreate(print,(void *) 13);
-        UserThreadCreate(print,(void *) 22);
+        int t1 = UserThreadCreate(print,(void *) 0);
+        UserThreadCreate(print,(void *) 1);
+        UserThreadCreate(print,(void *) 2);
+        UserThreadJoin(t1);
+        SynchPutString("<<Main waited>>\n");
+        UserThreadCreate(print,(void *) 4);
+
+/*        UserThreadCreate(print,(void *) 19);
         UserThreadCreate(print,(void *) 17);
         UserThreadCreate(print,(void *) 19);
-
+        UserThreadCreate(print,(void *) 17);
+        UserThreadCreate(print,(void *) 19);*/
 //ISSUE : More than three threads there is a problem in the display
 
         Halt();
